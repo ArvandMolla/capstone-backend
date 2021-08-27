03819793aced1da72469b2437771f502fe39a550
Arvand Molla <mr.arvand@gmail.com>
2021-08-27 15:30:58 +0200

    filters completed

--- src/api/adsAPI.js
+++ src/api/adsAPI.js
@@ -1,7 +1,7 @@
 import express from "express";
 import adModel from "../models/ad.js";
-
 import createError from "http-errors";
+import q2m from "query-to-mongo";
 
 const adRouter = express.Router();
 
@@ -17,12 +17,48 @@ adRouter.post("/post", async (req, res, next) => {
 
 adRouter.get("/", async (req, res, next) => {
   try {
-    const data = await adModel.find();
+    const data = await adModel.find().sort({ updatedAt: -1 });
     if (data) {
       res.status(200).send(data);
     } else {
       res.status(200).send("no ad is available");
     }
+  } catch (error) {
+    next(createError(500, error));
+  }
+});
+
+adRouter.get("/result", async (req, res, next) => {
+  try {
+    const query = q2m(req.query);
+    console.log(query);
+
+    let criteria = {};
+    if (req.query.search) {
+      criteria.$or = [
+        { title: { $regex: req.query.search, $options: "i" } },
+        { transcript: { $regex: req.query.search, $options: "i" } },
+      ];
+    }
+    if (req.query.labels) {
+      criteria.labels = {
+        $all: req.query.labels.split(","),
+      };
+    }
+    if (req.query.brand) {
+      criteria.brand = query.criteria.brand;
+    }
+
+    const data = await adModel
+      .find(criteria)
+      .sort({ updatedAt: -1 })
+      .skip(query.options.skip)
+      .limit(query.options.limit);
+
+    const total = await adModel.countDocuments(criteria);
+    console.log("this is criteria:  ", criteria);
+
+    res.send({ total, links: query.links("/result", total), data });
   } catch (error) {
     next(createError(500, error));
   }
