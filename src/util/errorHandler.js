bba94d5a16048b186889b82a969e9834d4f0a67e
Arvand Molla <mr.arvand@gmail.com>
2021-09-01 11:11:12 +0200

    403 error handler

--- src/util/errorHandler.js
+++ src/util/errorHandler.js
@@ -4,7 +4,9 @@ export const errorHandler = (err, req, res, next) => {
   } else if (err.status === 400) {
     res.status(400).send(err.errorList);
   } else if (err.status === 401) {
-    res.status(401).send(err.message);
+    res.status(401).send(err.message || "You are not logged in!");
+  } else if (err.status === 403) {
+    res.status(403).send(err.message || "You are not allowed!");
   } else {
     console.log("from errorHandler: ", err);
     res.status(500).send("generic server error!");
